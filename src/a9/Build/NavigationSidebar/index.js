fe0bb43c59a4203cecf3bf5618a344ff836d0e6f
nongzexi2021 <nzxnongzexi@outlook.com>
2021-12-07 10:38:29 -0800

    fix link

--- src/a9/Build/NavigationSidebar/index.js
+++ src/a9/Build/NavigationSidebar/index.js
@@ -83,7 +83,7 @@ const NavigationSidebar = ({active}) => {
                     </div>
                 </a>
 
-                <a href="/a9/twitter/profile" className={`list-group-item ${active === 'profile' ? 'active' :''}`}>
+                <Link to="/a9/twitter/profile" className={`list-group-item ${active === 'profile' ? 'active' :''}`}>
                     <div className="row">
                         <div className="col-2">
                             <div>
@@ -94,7 +94,7 @@ const NavigationSidebar = ({active}) => {
                             <div>Profile</div>
                         </div>
                     </div>
-                </a>
+                </Link>
 
                 <a href="more.html" className={`list-group-item ${active === 'more' ? 'active' :''}`}>
                     <div className="row">
